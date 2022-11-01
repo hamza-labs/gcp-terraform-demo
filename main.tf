6658852178c1a2e56c4362ff2adc782da6b804da
hamza <hamzaelghoujdami@gmail.com>
2022-11-01 23:26:16 +0000

    YCIT21 class 6

--- main.tf
+++ main.tf
@@ -77,11 +77,16 @@ resource "random_id" "bucket_prefix" {
 }
 
 resource "google_storage_bucket" "default" {
-  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  name = "bucket-tfstate-${random_id.instance_id.hex}"
   force_destroy = false
   location      = "US"
   storage_class = "STANDARD"
   versioning {
     enabled = true
   }
+}
+
+// Terraform plugin for creating random IDs
+resource "random_id" "instance_id" {
+  byte_length = 8
 }
